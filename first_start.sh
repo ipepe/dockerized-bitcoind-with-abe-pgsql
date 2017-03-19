6e348b98fa2f218f657d2546bef76d8ba7cc2e79
Patryk Ptasiński <patryk.ptasinski@ipepe.pl>
2017-03-19 01:06:02 +0100

    Missing volume mounting

--- first_start.sh
+++ first_start.sh
@@ -7,6 +7,6 @@ else
     echo docker build -t ipepe/$1 ./src
     docker build -t ipepe/$1 ./src
 
-    echo docker run -d --name $1 -h $1 --restart=unless-stopped -i -t -P ipepe/$1
-    docker run -d --name $1 -h $1 --restart=unless-stopped -i -t -P ipepe/$1
+    echo docker run -d --name $1 -h $1 --restart=unless-stopped -i -t -P -v /opt/docker/$1/data:/data ipepe/$1
+    docker run -d --name $1 -h $1 --restart=unless-stopped -i -t -P -v /opt/docker/$1/data:/data ipepe/$1
 fi
